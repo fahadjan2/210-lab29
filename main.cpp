2a8d3a2bd99090291cf9507b51173cf8e018f199
fahadjan2 <fahadfawad2006@gmail.com>
2025-11-05 19:27:46 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
 //Headers and standard library inclusions
 
-//Display Function that will go through the restaurant's current data and 
+//Display Function that will go through the restaurant's current data and display it neatly
+
+//Main function
+    //Create restaurant map
+    //Create lists in the map
+
+    //File open
+    //Error if file doesn't open
+    //Read data from the file
+        //For every line in the file, populate the menu, employees, and purchases made. Note every blank line will move on to the next data point 
+    //File close
+
+    //
+
+//End of main function
